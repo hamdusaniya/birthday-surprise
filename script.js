5ce5200981325bb8a211fab3d20f68bc85a10114
Ahmad Raza <ar26112003ar@gmail.com>
2026-07-10 15:50:14 +0530

    Enhance button click event with opacity effect
    Refactor button click event to include opacity transition before navigation.

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const button = document.getElementById("openBook");
 
-if (button) {
-    button.addEventListener("click", function () {
+button.addEventListener("click", function(){
+
+    document.body.style.opacity = "0";
+
+    setTimeout(function(){
+
         window.location.href = "gallery.html";
-    });
-}
+
+    },800);
+
+});
