2b3f2d7c56dfc8c1e017634d8b139ffcca7169df
Ahmad Raza <ar26112003ar@gmail.com>
2026-07-10 18:30:51 +0530

    Refactor photo gallery functionality and variable names

--- gallery.js
+++ gallery.js
@@ -1,28 +1,36 @@
 const photos=[
+
 "photo1.jpg",
+
 "photo2.jpg",
+
 "photo3.jpg",
+
 "photo4.jpg",
+
 "photo5.jpg"
+
 ];
 
 let current=0;
 
-const img=document.getElementById("photo");
+const photo=document.getElementById("photo");
 
-document.getElementById("nextPhoto").onclick=function(){
+const button=document.getElementById("nextPhoto");
 
-img.style.opacity="0";
+button.onclick=function(){
 
-setTimeout(function(){
+photo.style.opacity="0";
+
+setTimeout(()=>{
 
 current++;
 
 if(current<photos.length){
 
-img.src=photos[current];
+photo.src=photos[current];
 
-img.style.opacity="1";
+photo.style.opacity="1";
 
 }else{
 
