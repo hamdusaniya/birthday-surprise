60078b3f4ca7c6018488a0fb3d2ebedaba101de0
Ahmad Raza <ar26112003ar@gmail.com>
2026-07-10 15:21:23 +0530

    Add message display functionality on window load

---
+++ final.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+
+    setTimeout(function () {
+
+        document.getElementById("message1").classList.add("show");
+
+    }, 2000);
+
+    setTimeout(function () {
+
+        document.getElementById("message2").classList.add("show");
+
+    }, 5000);
+
+};
